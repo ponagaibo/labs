4ddec1c520afef8168b4a3be36032b448e783f48
ponagaibo <ponagaibo@mail.ru>
2016-11-30 21:59:27 +0400

    TL 15t

--- 4da.cpp
+++ 4da.cpp
@@ -4,10 +4,15 @@
 #include <iostream>
 #include <stdio.h>
 #include <algorithm>
+#include <ctime>
+
 unsigned int pat_len = 0;
-bool full = 1;
+unsigned int BUFSIZE = 0;
+unsigned int RANGE = 8;
+bool full = 0;
 unsigned int countStr = 1;
 unsigned int countWord = 0;
+unsigned int tot = 0;
 
 using std::cin; using std::cout; using std::endl; using std::string;
 
@@ -16,11 +21,11 @@ void PrintVect(const std::vector<std::pair<unsigned int, std::pair<unsigned int,
 	//cout << "size of vector: " << textBuf.size() << endl;
 	for(unsigned int i = 0; i < textBuf.size(); i++) {
 		unsigned int sp;
-        std::cout << "[ " << textBuf.at(i).first << ", " << (textBuf.at(i).second).first
-		<< ' ' << (textBuf.at(i).second).second << " ] ";
-		sp = (textBuf.at(i).second).second;
-		if (i < textBuf.size() - 1 && sp != (textBuf.at(i + 1).second).second) {
-			sp = (textBuf.at(i + 1).second).second;
+        std::cout << "[ " << textBuf[i].first << ", " << (textBuf[i].second).first
+		<< ' ' << (textBuf[i].second).second << " ] ";
+		sp = (textBuf[i].second).second;
+		if (i < textBuf.size() - 1 && sp != (textBuf[i + 1].second).second) {
+			sp = (textBuf[i + 1].second).second;
 			cout << '\n';
 		}
     }
@@ -31,7 +36,7 @@ void ZFunc(std::vector<std::pair<unsigned int, std::pair<unsigned int,unsigned i
 std::vector<std::pair<unsigned int,unsigned int> >& res) {
 	unsigned int n;
 	if (full) {
-		n = 6 * pat_len;
+		n = RANGE * pat_len;
 	} else {
 		n = vec.size();
 	}
@@ -41,33 +46,19 @@ std::vector<std::pair<unsigned int,unsigned int> >& res) {
 	unsigned int r = 0;
 
 	for (unsigned int i = 1; i < n; ++i) {
-		//std::cout << "TEXT:" << std::endl;
-		//PrintVect(vec);
-		//std::cout << std::endl;
-		//std::cout << "i = " << i << ", z[i] = " << z.at(i) << ", l = " << l << ", r = " << r << endl;
-
-		int min = std::min(r - i, z.at(i - l));
-		z.at(i) = std::max( 0, min );
-		while (i + z.at(i) < n && vec.at(z.at(i)).first == vec.at(i + z.at(i)).first) {
-			//cout << "while equal, z[i] = " << z[i] << ", i = " << i << endl;
-			++z.at(i);
+		int min = std::min(r - i, z[i - l]);
+		z[i] = std::max( 0, min );
+		while (i + z[i] < n && vec[z[i]].first == vec[i + z[i]].first) {
+			++z[i];
 		}
 
-		if (i + z.at(i) <= r) {
+		if (i + z[i] <= r) {
             l = i;
-            r = i + z.at(i) - 1;
-			//cout << "new border, l = " << i << ", r = " << r << endl;
+            r = i + z[i] - 1;
         }
-        if (z.at(i) >= pat_len && i >= pat_len) {
-			//cout << "found inclusion! at i = " << i << ", z[i] = " << z.at(i) << endl;
-            res.push_back( std::make_pair( (vec.at(i).second).first, (vec.at(i).second).second ));
+        if (z[i] >= pat_len && i >= pat_len) {
+            res.push_back( std::make_pair( (vec[i].second).first, (vec[i].second).second ));
         }
-/*
-		std::cout << "print z" << std::endl;
-		for(int i = 0; i < z.size(); i++)
-			std::cout << z[i] << ' ';
-		std::cout << std::endl;
-*/
 	}
 }
 
@@ -75,7 +66,7 @@ void GetPattern(std::vector<std::pair<unsigned int, std::pair<unsigned int,unsig
 	bool severalSpaces = 1;
 	unsigned int word = 0;
 	char c;
-	scanf("%c", &c);
+	c = getchar_unlocked();
 	while (c != '\n') {
 		if (c >= '0' && c <= '9') {
 			word *= 10;
@@ -88,23 +79,23 @@ void GetPattern(std::vector<std::pair<unsigned int, std::pair<unsigned int,unsig
 			severalSpaces = 1;
 			word = 0;
 		}
-		scanf("%c", &c);
+		c = getchar_unlocked();
 	}
 	if (!severalSpaces) {
 		pttrn.push_back(std::make_pair(word, std::make_pair(0,0)));
 	}
-	//cout << "PATTERN: " << endl;
-	//PrintVect(pttrn);
 }
 
 void GetText(std::vector<std::pair<unsigned int,
 			 std::pair<unsigned int,unsigned int> > >& textBuf,
 			 unsigned int bufferSize) {
+	//unsigned int start = clock();
 	unsigned int word = 0;
+	full = 0;
+	unsigned int size = textBuf.size();
 	bool severalDelim = 1;
 	char c;
-	unsigned int size = textBuf.size();
-	while (scanf("%c", &c) != EOF && size != bufferSize) {
+	while ((c = getchar_unlocked()) != EOF) {
 		if (c >= '0' && c <= '9') {
 			word *= 10;
 			word += (c - '0');
@@ -115,6 +106,10 @@ void GetText(std::vector<std::pair<unsigned int,
 			++size;
 			word = 0;
 			severalDelim = 1;
+			if (size == bufferSize) {
+				full = 1;
+				break;
+			}
 		} else if (c == '\n') {
 			if (!severalDelim) {
 				++countWord;
@@ -125,31 +120,36 @@ void GetText(std::vector<std::pair<unsigned int,
 			++countStr;
 			word = 0;
 			countWord = 0;
+			if (size == bufferSize) {
+				full = 1;
+				break;
+			}
 		}
 	}
-	if (size < bufferSize) {
-		full = 0;
-	} else {
-		ungetc(c, stdin);
-	}
+	//unsigned int end = clock();
+	//tot += (end - start);
 }
 
 int main() {
+	//unsigned int start = clock();
 	std::vector<std::pair<unsigned int, std::pair<unsigned int,unsigned int> > > pat;
 	GetPattern(pat);
 	pat_len = pat.size();
-	unsigned int bufferSize = 8 * pat.size();
+	unsigned int bufferSize = BUFSIZE * pat.size();
+	pat.reserve(bufferSize);
 	GetText(pat, bufferSize);
 	std::vector< std::pair<unsigned int,unsigned int> > res;
 	while (full) {
 		ZFunc(pat, res);
-		pat.erase(pat.begin() + pat_len, pat.begin() + 6 * pat_len);
+		pat.erase(pat.begin() + pat_len, pat.begin() + RANGE * pat_len);
 		GetText(pat, bufferSize);
 	}
 	ZFunc(pat, res);
-	//cout << "inclusions: " << endl;
 	for(unsigned int i = 0; i < res.size(); i++) {
-		std::cout << res.at(i).second << ',' << res.at(i).first << endl;
+		std::cout << res[i].second << ',' << res[i].first << endl;
 	}
+	//unsigned int end = clock();
+	//std::cout << "TIME: " << end-start << " ms." << std::endl;
+	//std::cout << "GetText: " << tot << " ms." << std::endl;
     return 0;
 }
